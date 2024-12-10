d65a6db6b78cec059b0ead602a123b998df495c5
Rima Nopa Utami <novaultammi11@gmail.com>
2024-12-09 20:59:10 -0800

    Add files via upload

---
+++ 14-LatihanPercabangan/main.py
@@ -0,0 +1,9 @@
+#  masukkan total belanja
+total_belanja = int(input("masukkan hasil belanja "))
+
+# logika untuk menentukan hadiah
+if total_belanja > 500000:
+    print("mouse an keyboard")
+else:
+    print("gantungan kunci")
+
---
+++ 15-PercabanganBersarang/main.py
@@ -0,0 +1,64 @@
+#Percabangan bersarang/ Nested IF
+
+#Kalkulator
+# +
+
+print(20*"=")
+print("kalkulator sederhana")
+print(20*"=")
+
+angka_1 = float(input("masukkan bilangan 1 = : "))
+opeator = input("opertor(+,-,/,x,%,//,**) = ")
+angka_2 = float(input("masukkan bilangan 2 = : "))
+
+
+# percabangan bersarang (elif statement)
+
+if operator == '+' :
+    hasil = angka_1 + angka_2
+    print(f' hasilnya adalah = {hasil}')
+elif operator == '-' :
+    hasil = angka_1 - angka_2
+    print(f' hasilnya adalah = {hasil}')
+elif operator == 'x' or operator == '*':
+    hasil = angka_1 * angka_2
+    print(f' hasilnya adalah = {hasil}')
+elif operator == '/' :
+    hasil = angka_1 / angka_2
+    print(f' hasilnya adalah = {hasil}')
+elif operator == '%' :
+    hasil = angka_1 % angka_2
+    print(f' hasilnya adalah = {hasil}')
+elif operator == '//' :
+    hasil = angka_1 // angka_2
+    print(f' hasilnya adalah = {hasil}')
+elif operator == '' :
+    hasil = angka_1 ** angka_2
+    print(f' hasilnya adalah = {hasil}')
+print("terimakasih")
+
+
+# percabangan bersarang (elif statement)
+
+if perator == '+' :
+    hasil = angka_1 + angka_2
+    print(f' hasilnya adalah = {hasil}')
+elif operator == '-' :
+    hasil = angka_1 - angka_2
+    print(f' hasilnya adalah = {hasil}')
+elif operator == 'x' or operator == '*':
+    hasil = angka_1 * angka_2
+    print(f' hasilnya adalah = {hasil}')
+elif operator == '/' :
+    hasil = angka_1 / angka_2
+    print(f' hasilnya adalah = {hasil}')
+elif operator == '%' :
+    hasil = angka_1 % angka_2
+    print(f' hasilnya adalah = {hasil}')
+elif operator == '//' :
+    hasil = angka_1 // angka_2
+    print(f' hasilnya adalah = {hasil}')
+elif operator == '' :
+    hasil = angka_1 ** angka_2
+    print(f' hasilnya adalah = {hasil}')
+print("terimakasih")
