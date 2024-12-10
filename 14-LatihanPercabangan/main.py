db63bdea4ab6d7c6f522dd9a4f88496d49bbd03d
Rima Nopa Utami <novaultammi11@gmail.com>
2024-12-09 21:02:14 -0800

    Delete 14-LatihanPercabangan directory

--- 14-LatihanPercabangan/main.py
+++
@@ -1,9 +0,0 @@
-#  masukkan total belanja
-total_belanja = int(input("masukkan hasil belanja "))
-
-# logika untuk menentukan hadiah
-if total_belanja > 500000:
-    print("mouse an keyboard")
-else:
-    print("gantungan kunci")
-
